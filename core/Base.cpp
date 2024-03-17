ef8a033acb834c9a111cd23532023223ef5c4ce8
nianzhou <nianzhou.ji@qq.com>
2024-03-17 16:42:23 +0800

    first commit

---
+++ core/Base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ji on 3/17/24.
+//
+
+#include "Base.hpp"
---
+++ core/Base.hpp
@@ -0,0 +1,31 @@
+//
+// Created by ji on 3/17/24.
+//
+
+#ifndef INC_02_SRC_BASE_HPP
+#define INC_02_SRC_BASE_HPP
+
+/**
+ * @brief
+ * initializes Opengl and configures and displays the application window
+ * - Startup: During this stage, objects are created, values are initialized, and any required external files are loaded.
+ * - Main Loop: This stage repeats continuously (typically 60 times per second) while the application is running, and
+ *   consists of the following three substages:
+ *      -# Process Input: Check if the user has performed any action that sends data to the computer,
+ *        such as pressing keys on a keyboard or clicking buttons on a mouse.
+ *      -# Update: Changing values of variables and objects.
+ *      -# Render: Create graphics that are displayed on the screen.
+ *
+ * - Shutdown: This stage typically begins when the user performs an action indicating
+ * that the program should stop running (for example, by clicking a button to quit the
+ * application). This stage may involve tasks such as signaling the application to stop
+ * checking for user input and closing any windows that were created by the application.
+ */
+class Base {
+
+
+
+};
+
+
+#endif //INC_02_SRC_BASE_HPP
